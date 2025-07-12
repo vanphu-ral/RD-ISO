66fb65a064c7bd6dba5c691284784ac761ebe3a4
QuangVinh <vinhls2002@gmail.com>
2025-07-12 11:40:58 +0700

    commit

--- src/test/java/com/mycompany/myapp/domain/CheckTargetTestSamples.java
+++ src/test/java/com/mycompany/myapp/domain/CheckTargetTestSamples.java
@@ -10,31 +10,17 @@ public class CheckTargetTestSamples {
     private static final AtomicLong longCount = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
 
     public static CheckTarget getCheckTargetSample1() {
-        return new CheckTarget()
-            .id(1L)
-            .name("name1")
-            .inspectionTarget("inspectionTarget1")
-            .evaluationLevelId(1L)
-            .status("status1")
-            .updateBy("updateBy1");
+        return new CheckTarget().id(1L).name("name1").status("status1").updateBy("updateBy1");
     }
 
     public static CheckTarget getCheckTargetSample2() {
-        return new CheckTarget()
-            .id(2L)
-            .name("name2")
-            .inspectionTarget("inspectionTarget2")
-            .evaluationLevelId(2L)
-            .status("status2")
-            .updateBy("updateBy2");
+        return new CheckTarget().id(2L).name("name2").status("status2").updateBy("updateBy2");
     }
 
     public static CheckTarget getCheckTargetRandomSampleGenerator() {
         return new CheckTarget()
             .id(longCount.incrementAndGet())
             .name(UUID.randomUUID().toString())
-            .inspectionTarget(UUID.randomUUID().toString())
-            .evaluationLevelId(longCount.incrementAndGet())
             .status(UUID.randomUUID().toString())
             .updateBy(UUID.randomUUID().toString());
     }
